bcb8891c953138f1b7d464ff0de4c19c507687f0
cristianciclo <cristiandelossantosciclo@gmail.com>
2017-02-20 09:48:58 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+  class script:
+      array = []
+      cadena = input('Introduzca una palabra: ')
+      print (cadena, '--', len(cadena))
+     print (cadena[::-1])
+      #creando array de strings
+       array=["casa","coche","autobus","instituto","luna"]
+       for var in array:
+        print (var)
